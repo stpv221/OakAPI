5d9210a50a43b3922ad2e0dde647301549a1ab8e
stpv221 <167645243+stpv221@users.noreply.github.com>
2024-04-26 23:38:05 +0000

    set up plans for OakAPI

---
+++ Oak.js
@@ -0,0 +1,9 @@
+const OakAPI = {
+    version: " infdev v1.0",
+    displayVersion: function () {
+        ModAPI.drawStringWithShadow({msg: "OakAPI " + OakAPI.version});
+    },
+    gameaAlert: function (opts) { },
+    sendKeybind: function (opts) { },
+    lwglKeys: {},
+};
