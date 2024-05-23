9737ea0d09cbf86db462551e7e693568d8e5b086
stpv22 <167645243+stpv221@users.noreply.github.com>
2024-05-23 10:30:00 -0700

    I'm adding an entire list of items :bj:

--- other mods/OneItem.js
+++ other mods/OneItem.js
@@ -1,6 +1,19 @@
 (() =>{
+    //a list of all possible givable items
+    var allItems = [0, 
+        {Name: "stone", Variants: [0, "{Damage:1}", "{Damage:1}", "{Damage:1}", "{Damage:1}", "{Damage:1}", "{Damage:1}"]}, //granite, 
+        {Name: "grass", Variants: null}, 
+        {Name: "dirt", Variants: [0, "{Damage: 1}", "{Damage: 2}"]}, //coarse, podzol
+        {Name: "cobblestone", Variants: null}];
+
     var GUIbutton = document.createElement("button");
-    GUIbutton.style="position: absolute; top:1rem; left:1rem;";
+    GUIbutton.style="position: fixed; top:1rem; left:1rem;";
+    GUIbutton.addEventListener("click", () => {
+        var GUIContainer = document.createElement("div");
+
+        document.body.appendChild(GUIContainer);
+    });
 
     //todo
+    document.body.appendChild(GUIbutton);
 })
